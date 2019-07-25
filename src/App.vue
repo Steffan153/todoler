8d12a299b3ff6190726bfe9bccd2902363212d6a
Caleb Miller <caleb@mathmammoth.com>
2019-07-25 13:29:38 -0600

    add install banner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <h1>todoler</h1>
     <Todos />
+    <div class="banner" v-show="banner">
+      To use this app offline and get here with a click of a button, install our free app:
+      <button @click="banner.prompt()">Install</button>
+    </div>
   </div>
 </template>
 
@@ -10,8 +14,19 @@ import Todos from "./components/Todos.vue";
 
 export default {
   name: "app",
+  data() {
+    return {
+      banner: null
+    };
+  },
   components: {
     Todos
+  },
+  mounted() {
+    window.addEventListener('beforeinstallprompt', (function(e) {
+      e.preventDefault();
+      this.banner = e;
+    }).bind(this));
   }
 };
 </script>
@@ -33,9 +48,33 @@ body {
 }
 </style>
 
-<style scoped>
+<style lang="scss" scoped>
 h1 {
   font-size: 3.5rem;
   text-align: center;
+}
+.banner {
+  background-color: blueviolet;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  padding: 10px;
+  width: 100%;
+  color: white;
+  button {
+    cursor: pointer;
+    text-decoration: none;
+    font: inherit;
+    color: white;
+    padding: 3px 20px;
+    background-color: blue;
+    border-radius: 10px;
+    transition: background-color .3s;
+    border: 2px solid darkblue;
+    outline: none;
+    &:hover {
+      background-color: darkblue;
+    }
+  }
 }
 </style>
